b6114c715e641235043f430a7fc430b123c9f6fb
Nitish Girkar <nitishlgirkar@gmail.com>
2026-07-13 00:01:12 +0800

    sss

--- models/silver/stg_agents.sql
+++
@@ -1,9 +0,0 @@
-with bronze as (
-    select * from {{ ref('bronze_agents') }}
-)
-select
-    trim(agent_id)             as agent_id,
-    initcap(trim(agent_name))  as agent_name,
-    trim(team)                 as team,
-    cast(hire_date as date)    as hire_date
-from bronze
--- models/silver/stg_clients.sql
+++
@@ -1,9 +0,0 @@
-with bronze as (
-    select * from {{ ref('bronze_clients') }}
-)
-select
-    trim(client_id)              as client_id,
-    initcap(trim(client_name))   as client_name,
-    trim(industry)               as industry,
-    cast(onboard_date as date)   as onboard_date
-from bronze
